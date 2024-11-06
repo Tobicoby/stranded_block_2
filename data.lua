3ae8bada82a0ba8b3795ce887cf6e4116001e2eb
Tobicoby <tobiasweiss20062112@gmail.com>
2024-11-06 19:09:22 +0100

    ver+, oil-processing reseachable

--- data.lua
+++ data.lua
@@ -11,6 +11,40 @@ data.raw['noise-expression']['cliffiness'].expression = 0
 table.insert(data.raw["technology"]["uranium-processing"].effects, {type = "unlock-recipe", recipe = "uranium-extraction"})
 table.insert(data.raw["technology"]["oil-processing"].effects, {type = "unlock-recipe", recipe = "oil-extraction"})
 
+data:extend({
+	{
+		type = "technology",
+		name = "oil-processing",
+		icon = "__base__/graphics/technology/oil-processing.png",
+		icon_size = 256,
+		prerequisites = {"oil-gathering"},
+		effects =
+		{
+			{
+			type = "unlock-recipe",
+			recipe = "oil-refinery"
+			},
+			{
+			type = "unlock-recipe",
+			recipe = "chemical-plant"
+			},
+			{
+			type = "unlock-recipe",
+			recipe = "basic-oil-processing"
+			},
+			{
+			type = "unlock-recipe",
+			recipe = "solid-fuel-from-petroleum-gas"
+			}
+		},
+		research_trigger =
+		{
+			type = "craft-item",
+			item = "pumpjack"
+		}
+	},
+})
+
 data.raw.recipe['burner-mining-drill'].enabled = false
 data.raw.recipe['electric-mining-drill'].enabled = false
 data.raw.recipe['chemical-plant'].enabled = true
