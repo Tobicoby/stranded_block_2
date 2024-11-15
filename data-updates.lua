e96b9e0f6dad07931d1284c307296ec587f98449
Tobicoby <tobiasweiss20062112@gmail.com>
2024-11-15 16:26:41 +0100

    world gen fixes

--- data-updates.lua
+++ data-updates.lua
@@ -8,9 +8,36 @@ if data.raw["planet"]["nauvis"] then
         v.autoplace = {probability_expression = 0}
     end
 
-    for k,v in pairs(data.raw["simple-entity"]) do
-        v.autoplace = {probability_expression = 0}
+    nauvis_rocks = {"big-rock", "huge-rock", "big-sand-rock"}
+    for k,v in pairs(nauvis_rocks) do
+        data.raw["simple-entity"][v].autoplace = {probability_expression = 0}
     end
+end
+
+-- THIS IS STUPID (COPIED FROM planet-fulgora-map-gen)
+-- BUT IT SPAWNS SCRAP
+if data.raw["planet"]["fulgora"] then
+    require("__space-age__/prototypes/planet/planet-fulgora-map-gen")
+    data.raw.resource.scrap.autoplace =
+    {
+    control = "scrap",
+    order = "b",
+    probability_expression = "(control:scrap:size > 0)\z
+                                * (1 - fulgora_starting_mask)\z
+                                * (min((fulgora_structure_cells < min(0.1 * frequency, 0.05 + 0.05 * frequency))\z
+                                * (1 + fulgora_structure_subnoise) * abs_mult_height_over * fulgora_artificial_mask\z
+                                + (fulgora_spots_prebanding < (1.2 + 0.4 * linear_size)) * fulgora_vaults_and_starting_vault * 10,\z
+                                0.5) * (1 - fulgora_road_paving_2c))",
+    richness_expression = "(1 + fulgora_structure_subnoise) * 1000 * (7 / (6 + frequency) + 100 * fulgora_vaults_and_starting_vault) * richness",
+    local_expressions =
+    {
+        abs_mult_height_over = "fulgora_elevation > (fulgora_coastline + 10)",
+        frequency = "control:scrap:frequency",
+        size = "control:scrap:size",
+        linear_size = "slider_to_linear(size, -1, 1)",
+        richness = "control:scrap:richness"
+    }
+    }
 end
 
 data:extend(
